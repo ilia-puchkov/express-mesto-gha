0f738942e1068f24b37cb64b9ba5f03f9b334665
Ilia Puchkov <reras@yandex.ru>
2023-04-23 15:44:45 +0300

    dev: s14

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
-// const NotFoundError = require('../errors/NotFoundError');
+const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
-/*
-router.use(() => {
-  throw new NotFoundError('Страница не найдена');
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
 });
-*/
+
 module.exports = router;
