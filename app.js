d49810cd5eb836344f7cebf7012e90b276aa3546
ilia-puchkov <reras@yandex.ru>
2023-05-12 16:43:25 +0300

    fix 1

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const router = require('./routes');
+const cors = require('./middlewares/cors');
 
 const { customError } = require('./middlewares/customError');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -16,7 +19,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(requestLogger);
+
+app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
+
+app.use(errorLogger);
 app.use(errors());
 app.use(customError);
 
